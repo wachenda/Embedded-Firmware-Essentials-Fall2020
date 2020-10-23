2aa1aedaeb81092f5f759958e38c95c6b75701d4
David Wachenschwanz <dwachenschwanz@smartorg.com>
2020-10-23 16:41:21 -0700

    Updating Lect7_Test_New_NeoPixel

--- Lect7_Test_New_NeoPixel/Core/Src/main.c
+++ Lect7_Test_New_NeoPixel/Core/Src/main.c
@@ -28,19 +28,19 @@
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
 
-#define WS2812_FREQ												(800000) 			// it is fixed: WS2812 require 800kHz
-#define TIMER_CLOCK_FREQ									(32000000)   	// can be modified - multiples of 0.8MHz are suggested
-#define TIMER_PERIOD											(TIMER_CLOCK_FREQ / WS2812_FREQ)
+#define WS2812_FREQ			(800000)		// WS2812 requires 800kHz
+#define TIMER_CLOCK_FREQ	(32000000)
+#define TIMER_PERIOD		(TIMER_CLOCK_FREQ / WS2812_FREQ)
 
-#define LED_NUMBER												(16)					// how many LEDs the MCU should control?
-#define LED_DATA_SIZE											(LED_NUMBER * 24)
-#define RESET_SLOTS_BEGIN									(50)
-#define RESET_SLOTS_END										(50)
-#define WS2812_LAST_SLOT									(1)
-#define LED_BUFFER_SIZE										(RESET_SLOTS_BEGIN + LED_DATA_SIZE + WS2812_LAST_SLOT + RESET_SLOTS_END)
-#define WS2812_0													(TIMER_PERIOD / 3)				// WS2812's zero high time is long about one third of the period
-#define WS2812_1													(TIMER_PERIOD * 2 / 3)		// WS2812's one high time is long about two thirds of the period
-#define WS2812_RESET											(0)
+#define LED_NUMBER			(16)	// how many LEDs the MCU should control?
+#define LED_DATA_SIZE		(LED_NUMBER * 24) // 24 bits per LED
+#define RESET_SLOTS_BEGIN	(50)
+#define RESET_SLOTS_END		(50)
+#define WS2812_LAST_SLOT	(1)
+#define LED_BUFFER_SIZE		(RESET_SLOTS_BEGIN + LED_DATA_SIZE + WS2812_LAST_SLOT + RESET_SLOTS_END)
+#define WS2812_0			(TIMER_PERIOD / 3)				// WS2812's zero high time is long about one third of the period
+#define WS2812_1			(TIMER_PERIOD * 2 / 3)		// WS2812's one high time is long about two thirds of the period
+#define WS2812_RESET		(0)
 
 
 /* USER CODE END PTD */
@@ -578,7 +578,7 @@ static void MX_TIM1_Init(void)
   htim1.Instance = TIM1;
   htim1.Init.Prescaler = 0;
   htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim1.Init.Period = 59;
+  htim1.Init.Period = 39;
   htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim1.Init.RepetitionCounter = 0;
   htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
@@ -713,6 +713,15 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
+void HAL_TIM_PWM_PulseFinishedHalfCpltCallback(TIM_HandleTypeDef *htim)
+{
+	HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_RESET);
+}
+
+void HAL_TIM_PWM_PulseFinishedCallback(TIM_HandleTypeDef *htim)
+{
+	HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_SET);
+}
 
 /* USER CODE END 4 */
 
